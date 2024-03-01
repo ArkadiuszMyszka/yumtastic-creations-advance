f53b36109c956c713ba258d48e77fb64f9dc8559
Joanna Matwiej <126902190+Tworzaca0789@users.noreply.github.com>
2024-03-01 19:35:24 +0100

    Add private file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { refreshUser } from './redux/auth/authOperations.jsx';
-import { useAuth } from './hooks/useAuth.jsx';
+import useAuth from './hooks/useAuth.jsx';
 import { useDispatch } from 'react-redux';
-import { useEffect, lazy } from 'react';
-import { SharedLayout } from './Layout.jsx'; // do zmiany jak dojdzie plik
+import { useEffect } from 'react'; // dodac lazy
+// import { SharedLayout } from './Layout.jsx'; // do zmiany jak dojdzie plik
 import PrivateRoute from './components/PrivateRoute.jsx';
 import React from 'react';
 import RestrictedRoute from './components/RestrictedRoute.jsx';
@@ -10,18 +10,18 @@ import { Route, Routes } from 'react-router-dom';
 
 // do zmiany sciezki na bieżąco jak bedą pojawiac sie pliki
 
-const AddRecipesPage = React.lazy(() => import('pages/AddRecipes'));
-const CategoriesPage = React.lazy(() => import('pages/Categories'));
-const FavoritesPage = React.lazy(() => import('pages/Favorites'));
-const MainPage = React.lazy(() => import('pages/Main'));
-const MyRecipesPage = React.lazy(() => import('pages/MyRecipes'));
-const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
-const RecipesPage = React.lazy(() => import('pages/Recipe'));
-const RegisterPage = lazy(() => import('../pages/Register'));
-const SearchPage = React.lazy(() => import('pages/Search/Search'));
-const ShoppingListPage = React.lazy(() => import('pages/ShoppingList'));
-const SigninPage = lazy(() => import('../pages/Signin'));
-const WelcomePage = lazy(() => import('../pages/Welcome'));
+// const AddRecipesPage = React.lazy(() => import('pages/AddRecipes'));
+// const CategoriesPage = React.lazy(() => import('pages/Categories'));
+// const FavoritesPage = React.lazy(() => import('pages/Favorites'));
+// const MainPage = React.lazy(() => import('pages/Main'));
+// const MyRecipesPage = React.lazy(() => import('pages/MyRecipes'));
+// const NotFoundPage = lazy(() => import('pages/NotFoundPage'));
+// const RecipesPage = React.lazy(() => import('pages/Recipe'));
+// const RegisterPage = lazy(() => import('../pages/Register'));
+// const SearchPage = React.lazy(() => import('pages/Search/Search'));
+// const ShoppingListPage = React.lazy(() => import('pages/ShoppingList'));
+// const SigninPage = lazy(() => import('../pages/Signin'));
+// const WelcomePage = lazy(() => import('../pages/Welcome'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -38,55 +38,101 @@ const App = () => {
       <>
         <Route
           path="/welcome"
-          element={<RestrictedRoute component={<WelcomePage />} />}
+          element={
+            <RestrictedRoute
+            // component={<WelcomePage />}
+            />
+          }
         />
         <Route
           path="/register"
-          element={<RestrictedRoute component={<RegisterPage />} />}
+          element={
+            <RestrictedRoute
+            // component={<RegisterPage />}
+            />
+          }
         />
         <Route
           path="/signin"
-          element={<RestrictedRoute component={<SigninPage />} />}
+          element={
+            <RestrictedRoute
+            // component={<SigninPage />}
+            />
+          }
         />
       </>
 
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<PrivateRoute component={<MainPage />} />} />
+      <Route
+        path="/"
+        //   element={<SharedLayout />}
+      >
+        <Route
+          index
+          // element={<PrivateRoute component={<MainPage />} />}
+        />
+
         <Route
           path="categories/:categoryName"
           element={
             <PrivateRoute
               redirectTo="/welcome"
-              component={<CategoriesPage />}
+              // component={<CategoriesPage />}
             />
           }
         />
 
         <Route
           path="add"
-          element={<PrivateRoute component={<AddRecipesPage />} />}
+          element={
+            <PrivateRoute
+            // component={<AddRecipesPage />}
+            />
+          }
         />
         <Route
           path="my"
-          element={<PrivateRoute component={<MyRecipesPage />} />}
+          element={
+            <PrivateRoute
+            // component={<MyRecipesPage />}
+            />
+          }
         />
         <Route
           path="favorite"
-          element={<PrivateRoute component={<FavoritesPage />} />}
+          element={
+            <PrivateRoute
+            // component={<FavoritesPage />}
+            />
+          }
         />
         <Route
           path="shopping-list"
-          element={<PrivateRoute component={<ShoppingListPage />} />}
+          element={
+            <PrivateRoute
+            // component={<ShoppingListPage />}
+            />
+          }
         />
         <Route
           path="search"
-          element={<PrivateRoute component={<SearchPage />} />}
+          element={
+            <PrivateRoute
+            // component={<SearchPage />}
+            />
+          }
         />
         <Route
           path="recipe/:recipeId"
-          element={<PrivateRoute component={<RecipesPage />} />}
+          element={
+            <PrivateRoute
+            // component={<RecipesPage />}
+            />
+          }
         />
-        <Route path="*" element={<NotFoundPage />} />
+        <Route
+          path="*"
+          // element={<NotFoundPage />}
+        />
       </Route>
     </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/yummy-frontend">
+      <BrowserRouter basename="/yumtastic-creations-advance">
         <GlobalStyles />
         <App />
       </BrowserRouter>
